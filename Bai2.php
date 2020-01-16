216d099fbc8e9076903913bdd4cbb192933df217
vu9630 <58212148+vu9630@users.noreply.github.com>
2020-01-16 16:09:20 +0700

    Bt_buoi5

---
+++ Bai2.php
@@ -0,0 +1,72 @@
+<?php
+ 
+
+ function sumArray($arr) {
+     
+    $arr = array(
+        5,
+        5,
+        5,
+        5
+    );
+    echo "sum = " . array_sum($array);
+     
+ 
+
+$i = $argv;
+
+
+function isPerfectNumber($k){
+
+    $k = 0;
+
+   for ($i = 1; $i < $x; $i++) {
+
+       if ($x % $i) {
+           $k += $i;
+
+       if ($x == $k) {
+          return true;
+}
+       else {
+          return false;
+}
+     function check($x){
+
+        for ($x = 0; $x<$count($k); $x++)
+        
+           echo $n;
+     }
+
+
+
+
+
+ function sortArray($arr) {
+
+         for($i=2; $i<count($arr)-2; $i++){
+         
+             $min = $arr[$i];
+            $keyMin = $i;
+
+            for($j=$i+1; $j<count($arr);$j++){
+                if($arr[$j] < $min ){
+                   $min = $arr[$j];
+                    $keyMin = $j;
+
+                }
+                
+            }
+             $arr[$keyMin] = $arr[$i];
+
+            $arr[$i] = $min;
+            
+         }
+        print_r($arr);
+
+}
+ sortArray([2,1,3,5,6]);
+
+?>
+
+
---
+++ Bai3.php
@@ -0,0 +1,11 @@
+$n = $argv[1];
+
+meanScore($arr) {
+
+    $sinhVien = [“ten” => “Lê Văn Việt”, “gioiTinh” => 0, “tongDiem” => 25];
+foreach($sinhVien as $key => $value){
+echo $key. “\t” . $mang[$i] . “\n”;
+}
+
+print_r(explode(“,”, $str));     
+}
